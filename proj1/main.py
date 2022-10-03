05d4e6d3edf7c85ed34fb76723447cecf4a7626b
Diogo Santos <diogofilipe1_santos@hotmail.com>
2022-10-03 16:11:46 +0100

    started to develop identification

--- proj1/main.py
+++ proj1/main.py
@@ -1,32 +1,107 @@
 import cv2 as cv
+import numpy as np
 import game.game as gamePackage
 import camera.remoteWebCam as remoteWebCamPackage
+import game.cards as cards
+
+cardImages = {cards.ACE_SPADES: {"fileName": "./cards_normal/1.png"},
+              cards.ACE_HEARTS: {"fileName": "./cards_normal/2.png"},
+              cards.ACE_CLUBS: {"fileName": "./cards_normal/3.png"},
+              cards.ACE_DIAMONDS: {"fileName": "./cards_normal/4.png"}}
+
+def setUp():
+    for card in cardImages:
+        img = cv.imread(cardImages[card]["fileName"])
+        cardImages[card]["img"] = img
+
+MIN_AREA_OF_CARDS = 5000
 
 def binarize(img, thresholdValue = 127):
-    binarized = cv.threshold(img, thresholdValue, 255, cv.THRESH_BINARY)
+    grayScaleImg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    _, binarized = cv.threshold(grayScaleImg, thresholdValue, 255, cv.THRESH_BINARY)
     return binarized
 
 def detectConnectedComponents(img):
-    return img
+    numLabels, labels, stats, _ = cv.connectedComponentsWithStats(img)
+    
+    connectedComponents = []
+    for i in range(1, numLabels):            
+        if stats[i, cv.CC_STAT_AREA] >= MIN_AREA_OF_CARDS:
+            componentMask = (labels == i).astype("uint8") * 255
+            # componentMask = fillComponent(componentMask, 255)
+            connectedComponents.append(componentMask)
+    
+    return connectedComponents
 
-def detectContours(img):
-    return img
+def fillComponent(component, fillColor):
+    for i in range(len(component)):
+        # find first index different from 0
+        firstIndex = -1
+        for j in range(len(component[i])):
+            if component[i][j] != 0:
+                firstIndex = j
+                
+        # find last index different from 0
+        lastIndex = -1
+        for j in range(len(component[i]) - 1, -1, -1):
+            if component[i][j] != 0:
+                lastIndex = j
+                
+        component[i][firstIndex : lastIndex + 1] = fillColor
+        
+    return component
 
-def detectQuadrilaterals(img):
-    return img
+def detectQuadrilaterals(imgShape, components):    
+    quadrilaterals = np.zeros(imgShape, dtype="uint8")
+    
+    for component in components:
+        contours, _ = cv.findContours(image=component, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)
+        numberOfSides = cv.approxPolyDP(contours[0], 0.04 * cv.arcLength(contours[0], True), True)
+    
+        # if len(numberOfSides) == 4:
+        quadrilaterals = cv.bitwise_or(quadrilaterals, component)
+            
+    return quadrilaterals
 
 # return: img with only the cards
 def detectCards(img):
-    binarized = binarize(img)
+    binarized = binarize(img, 100)
     connectedComponents = detectConnectedComponents(binarized)
-    contours = detectContours(connectedComponents)
-    cards = detectQuadrilaterals(contours)
+    quadrilaterals = detectQuadrilaterals(binarized.shape, connectedComponents)
+    cards = cv.bitwise_and(img, img, mask = quadrilaterals)
     
     return cards
 
+# using FEATURE MATCHING
 # return: {cardName: img}
-def identifyCards(cards):
-    return []
+def identifyCards(round):
+    detectedCard = None
+    numberOfMatches = -1
+    
+    sift = cv.SIFT_create()
+    
+    _, des1 = sift.detectAndCompute(round, None)
+    for card in cardImages:
+        _, des2 = sift.detectAndCompute(cardImages[card]["img"], None)
+        
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        
+        flann = cv.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1, des2, k=2)
+        
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m,n in matches:
+            if m.distance < 0.7 * n.distance:
+                good.append(m)
+                
+        if len(good) > numberOfMatches:
+            detectedCard = card
+            numberOfMatches = len(good)
+        
+    print(f"Detected: {detectedCard}")
 
 # return: {cardName: playerName}
 # ---------------------------------
@@ -40,6 +115,10 @@ def identifyCards(cards):
 # ---------------------------------
 def associatePlayersWithCards(cards):
     pass
+
+# ==================================================================
+
+setUp()
 
 QUIT_KEY = ord("q")
 
@@ -65,7 +144,7 @@ while True:
     # game.gameRound(playersAssociatedWithEachCard)
     # roundWinner = game.getRoundWinner()
     
-    cv.imshow("video", frame)
+    cv.imshow("video", cards)
     
     key = cv.waitKey(1)
     if key == QUIT_KEY:
